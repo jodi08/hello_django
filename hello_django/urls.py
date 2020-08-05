6bfa98fea8712803406868f69fe68f688031ab3b
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-08-04 19:07:30 -0700

    changed some errors. Working now

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from hello_app.views import index_view
 
 urlpatterns = [
-    path('', include(hello_app.urls)),
+    path('', index_view),
     path('admin/', admin.site.urls),   
 ]
