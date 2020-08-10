a32e9d49838e992cf11a86a65608d63bc7ab46b4
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-08-10 09:27:46 -0700

    fix hard coding of Hello World and add variable in context dictionary.

--- hello_app/views.py
+++ hello_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"myvariable": "Hello, World!"})
